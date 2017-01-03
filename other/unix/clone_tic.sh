3ffbb88c3d75d4a448f9a4aa637e0816f90d2a9a
bkraft <bkraft4257@gmail.com>
2017-01-03 12:44:02 -0500

    Fixed bug.

--- other/unix/clone_tic.sh
+++ other/unix/clone_tic.sh
@@ -15,15 +15,13 @@ TIC_SETUP_PATH=$HOME/.tic
 
 git clone https://github.com/bkraft4257/tic_freesurfer
 git clone https://github.com/bkraft4257/tic_labels
-git clone https://github.com/bkraft4257/tic_outliers
 git clone https://github.com/crhamilt/tic_protocol_check
 git clone https://github.com/crhamilt/tic_redcap_link
 
 if [ ! -d $TIC_SETUP_PATH ]; then
 
-    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_freesurfer.sh $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_labels/other/unix/tic_labels.sh $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_outliers/other/unix/tic_outliers.sh $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_protocol_check/other/unix/tic_protocol_check.sh $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_redcap_link/other/unix/tic_redcap_link.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_tools/other/unix/{tic,tic_tools}.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/tic_freesurfer.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_redcap_link/other/tic_redcap_link.sh $TIC_SETUP_PATH
+
 fi
