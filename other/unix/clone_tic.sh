c3047f5274481f9fcb0eeeb5e49d235e60ca16fb
bkraft <bkraft4257@gmail.com>
2017-01-03 12:48:11 -0500

    Fixed bug.

--- other/unix/clone_tic.sh
+++ other/unix/clone_tic.sh
@@ -16,10 +16,11 @@ TIC_SETUP_PATH=$HOME/.tic
 git clone https://github.com/bkraft4257/tic_freesurfer
 git clone https://github.com/bkraft4257/tic_labels
 git clone https://github.com/crhamilt/tic_protocol_check
-git clone https://github.com/crhamilt/tic_redcap_link
+git clone https://github.com/bkraft/tic_redcap_link
 
 if [ ! -d $TIC_SETUP_PATH ]; then
 
+    mkdir $TIC_SETUP_PATH
     cp $TIC_REPOSITORY_PATH/tic_tools/other/unix/{tic,tic_tools}.sh $TIC_SETUP_PATH
     cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/tic_freesurfer.sh $TIC_SETUP_PATH
     cp $TIC_REPOSITORY_PATH/tic_redcap_link/other/tic_redcap_link.sh $TIC_SETUP_PATH
