5332543958b63c46a5b02f781730cd1ff27a52cf
bkraft <bkraft4257@gmail.com>
2017-01-03 12:40:07 -0500

    Fixed bug.

--- other/unix/clone_tic.sh
+++ other/unix/clone_tic.sh
@@ -13,16 +13,17 @@ TIC_REPOSITORY_PATH=${1-$PWD}
 TIC_SETUP_PATH=$HOME/.tic
 
 
-git clone https://github.com/bkraft4257/tic_outliers
-git clone https://github.com/crhamilt/tic_redcap_link
-git clone https://github.com/crhamilt/tic_protocol_check
+git clone https://github.com/bkraft4257/tic_freesurfer
 git clone https://github.com/bkraft4257/tic_labels
+git clone https://github.com/bkraft4257/tic_outliers
+git clone https://github.com/crhamilt/tic_protocol_check
+git clone https://github.com/crhamilt/tic_redcap_link
 
 if [ ! -d $TIC_SETUP_PATH ]; then
 
     cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_freesurfer.sh $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_labels.sh $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_outliers.sh $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_protocol_check.sh $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_redcap_link.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_labels/other/unix/tic_labels.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_outliers/other/unix/tic_outliers.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_protocol_check/other/unix/tic_protocol_check.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_redcap_link/other/unix/tic_redcap_link.sh $TIC_SETUP_PATH
 fi
