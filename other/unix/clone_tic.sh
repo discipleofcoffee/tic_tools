0f80408570706931a317cf3e43c07bb4f8f99225
bkraft <bkraft4257@gmail.com>
2017-01-03 12:35:35 -0500

    Added projects to clone. Also initialized the .tic directory

--- other/unix/clone_tic.sh
+++ other/unix/clone_tic.sh
@@ -12,4 +12,13 @@
 git clone https://github.com/bkraft4257/tic_outliers
 git clone https://github.com/crhamilt/tic_redcap_link
 git clone https://github.com/crhamilt/tic_protocol_check
-git clone https://github.com/bkraft4257/labels
+git clone https://github.com/bkraft4257/tic_labels
+
+if [ ! -d $TIC_SETUP_PATH ]; then
+
+    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_freesurfer.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_labels.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_outliers.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_protocol_check.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/unix/tic_redcap_link.sh $TIC_SETUP_PATH
+fi
--- tools/plot_overlay.py
+++ tools/plot_overlay.py
@@ -1,118 +1,191 @@
+#!/usr/bin/env python
+
 """
-3D and 4D niimgs: handling and visualizing
-==========================================
+create volume brain mask from pial surface and aseg
+"""
+from __future__ import division
 
-Here we discover how to work with 3D and 4D niimgs.
+import sys
+import argparse
+from nilearn import plotting, image
+
+
+"""
+Copied from https://gist.github.com/endolith/2879736
+
+Copyright 2012 endolith at gmail com
+Copyright 2009 Ged Ridgway at gmail com
+Translation and modification of
+http://www.mathworks.com/matlabcentral/fileexchange/26026-bipolar-colormap
+Based on Manja Lehmann's hand-crafted colormap for cortical visualisation
 """
 
-from nilearn import datasets
-from nilearn import plotting
 
-###############################################################################
-# Downloading tutorial datasets from Internet
-# --------------------------------------------
+from scipy.interpolate import interp1d
+import numpy
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+
+def bipolar(lutsize=256, n=0.333, interp='linear'):
+    """
+    Bipolar hot/cold colormap, with neutral central color.
+
+    This colormap is meant for visualizing diverging data; positive
+    and negative deviations from a central value.  It is similar to a
+    blackbody colormap for positive values, but with a complementary
+    "cold" colormap for negative values.
+
+    Parameters
+    ----------
+    lutsize : int
+        The number of elements in the colormap lookup table. (Default is 256.)
+    n : float
+        The gray value for the neutral middle of the colormap.  (Default is
+        1/3.)
+        The colormap goes from cyan-blue-neutral-red-yellow if neutral
+        is < 0.5, and from blue-cyan-neutral-yellow-red if neutral > 0.5.
+        For shaded 3D surfaces, an `n` near 0.5 is better, because it
+        minimizes luminance changes that would otherwise obscure shading cues
+        for determining 3D structure.
+        For 2D heat maps, an `n` near the 0 or 1 extremes is better, for
+        maximizing luminance change and showing details of the data.
+    interp : str or int, optional
+        Specifies the type of interpolation.
+        ('linear', 'nearest', 'zero', 'slinear', 'quadratic, 'cubic')
+        or as an integer specifying the order of the spline interpolator
+        to use. Default is 'linear'.  See `scipy.interpolate.interp1d`.
+
+    Returns
+    -------
+    out : matplotlib.colors.LinearSegmentedColormap
+        The resulting colormap object
+
+    Notes
+    -----
+    If neutral is exactly 0.5, then a map which yields a linear increase in
+    intensity when converted to grayscale is produced. This colormap should
+    also be reasonably good
+    for colorblind viewers, as it avoids green and is predominantly based on
+    the purple-yellow pairing which is easily discriminated by the two common
+    types of colorblindness. [2]_
+
+    Examples
+    --------
+    >>> from mpl_toolkits.mplot3d import Axes3D
+    >>> from matplotlib import cm
+    >>> import matplotlib.pyplot as plt
+    >>> import numpy as np
+    >>> fig = plt.figure()
+    >>> ax = fig.gca(projection='3d')
+    >>> x = y = np.arange(-4, 4, 0.15)
+    >>> x, y = np.meshgrid(x, y)
+    >>> z = (1- x/2 + x**5 + y**3)*exp(-x**2-y**2)
+    >>> surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, linewidth=0.1,
+    >>>                        vmax=abs(z).max(), vmin=-abs(z).max())
+    >>> fig.colorbar(surf)
+    >>> plt.show()
+    >>> set_cmap(bipolar(201))
+    >>> waitforbuttonpress()
+    >>> set_cmap(bipolar(201, 0.1)) # dark gray as neutral
+    >>> waitforbuttonpress()
+    >>> set_cmap(bipolar(201, 0.9)) # light gray as neutral
+    >>> waitforbuttonpress()
+    >>> set_cmap(bipolar(201, 0.5)) # grayscale-friendly colormap
+
+    References
+    ----------
+    .. [1] Lehmann Manja, Crutch SJ, Ridgway GR et al. "Cortical thickness
+        and voxel-based morphometry in posterior cortical atrophy and typical
+        Alzheimer's disease", Neurobiology of Aging, 2009,
+        doi:10.1016/j.neurobiolaging.2009.08.017
+    .. [2] Brewer, Cynthia A., "Guidelines for Selecting Colors for
+        Diverging Schemes on Maps", The Cartographic Journal, Volume 33,
+        Number 2, December 1996, pp. 79-86(8)
+        http://www.ingentaconnect.com/content/maney/caj/1996/00000033/00000002/art00002
+    """
+    if n < 0.5:
+        if not interp:
+            interp = 'linear'  # seems to work well with dark neutral colors  cyan-blue-dark-red-yellow
+
+        _data = (
+            (0, 1, 1),  # cyan
+            (0, 0, 1),  # blue
+            (n, n, n),  # dark neutral
+            (1, 0, 0),  # red
+            (1, 1, 0),  # yellow
+        )
+    elif n >= 0.5:
+        if not interp:
+            interp = 'cubic'  # seems to work better with bright neutral colors blue-cyan-light-yellow-red
+            # produces bright yellow or cyan rings otherwise
+
+        _data = (
+            (0, 0, 1),  # blue
+            (0, 1, 1),  # cyan
+            (n, n, n),  # light neutral
+            (1, 1, 0),  # yellow
+            (1, 0, 0),  # red
+        )
+    else:
+        raise ValueError('n must be 0.0 < n < 1.0')
+
+    xi = numpy.linspace(0, 1, numpy.array(_data).shape[0])
+    cm_interp = interp1d(xi, _data, axis=0, kind=interp)
+    xnew = numpy.linspace(0, 1, lutsize)
+    ynew = cm_interp(xnew)
+
+    # No form of interpolation works without this, but that means the interpolations are not working right.
+    ynew = numpy.clip(ynew, 0, 1)
+
+    return cm.colors.LinearSegmentedColormap.from_list('bipolar', ynew, lutsize)
+
+
+# ======================================================================================================================
+# region Main Function
 #
-# Nilearn comes with functions that download public data from Internet
+
+def plot_overlay(background, overlay, xyz_direction, ncuts, threshold=1, alpha=0.5, figure_name='figure1'):
+
+    cuts = plotting.find_cut_slices(image.load_img(background), direction=xyz_direction, n_cuts=ncuts, spacing='auto')
+
+    print(cuts)
+    n_slices = list(range(1,ncuts))
+    print(n_slices)
+
+
+    for ii,jj in enumerate(cuts):
+        plotting.plot_stat_map(overlay, bg_img=background, cmap=bipolar(), black_bg=True,
+                                        display_mode=xyz_direction, cut_coords=[jj], threshold=threshold, alpha = alpha,
+                                        output_file='overlay_{0}_{1:03}'.format(xyz_direction, ii) + '.png')
+
+#endregion
+
+# ======================================================================================================================
+# region Main Function
 #
-# Let's first check where the data is downloaded on our disk:
 
-print('Datasets are stored in: %r' % datasets.get_data_dirs())
+def main():
+    ## Parsing Arguments
+    #
+    #
 
-###############################################################################
-# Let's now retrieve a motor contrast from a localizer experiment
-tmap_filenames = datasets.fetch_localizer_button_task()['tmaps']
-print(tmap_filenames)
+    usage = "usage: %prog [options] arg1 arg2"
 
-###############################################################################
-# tmap_filenames is a list of filenames. We need to take the first one
-tmap_filename = tmap_filenames[0]
+    parser = argparse.ArgumentParser(prog='create_pial_mask')
 
+    parser.add_argument('background', help="Background Image")
+    parser.add_argument('overlay', help="Overlay Image")
+    parser.add_argument('--dir', help="Direction", choices=['x','y','z','ortho'], default='z')
+    parser.add_argument('--nslices', help="Number of slices",type=int, default=10)
 
-###############################################################################
-print('Visualizing a 3D file')
-# ----------------------
-#
-# The file contains a 3D volume, we can easily visualize it as a
-# statistical map:
-#
-# display = plotting.plot_stat_map(img)
-# display.savefig('pretty_brain.png')
-# Don't forget to close the display
-# display.close()
+    inArgs = parser.parse_args()
 
-display = plotting.plot_stat_map(tmap_filename)
-display.savefig('figure1.png')
+    plot_overlay(inArgs.background, inArgs.overlay, inArgs.dir, inArgs.nslices)
 
 
-###############################################################################
-# Visualizing works better with a threshold
-display = plotting.plot_stat_map(tmap_filename, threshold=3)
-display.savefig('figure2.png')
+#endregion
 
-###############################################################################
-# Visualizing one volume in a 4D file
-# -----------------------------------
-#
-# We can download resting-state networks from the Smith 2009 study on
-# correspondance between rest and task
-rsn = datasets.fetch_atlas_smith_2009()['rsn10']
-
-###############################################################################
-# It is a 4D nifti file. We load it into the memory to print its
-# shape.
-from nilearn import image
-print(image.load_img(rsn).shape)
-
-###############################################################################
-# We can retrieve the first volume (note that Python indexing starts at 0):
-first_rsn = image.index_img(rsn, 0)
-print(first_rsn.shape)
-
-###############################################################################
-print('first_rsn is a 3D image.')
-#
-# We can then plot it
-display = plotting.plot_stat_map(first_rsn)
-display.savefig('figure3.png')
-
-###############################################################################
-# Looping on all volumes in a 4D file
-# -----------------------------------
-#
-# If we want to plot all the volumes in this 4D file, we can use iter_img
-# to loop on them.
-#
-# Then we give a few arguments to plot_stat_map in order to have a more
-# compact display.
-for ii,img in enumerate(image.iter_img(rsn)):
-    print('###############################################################################')
-    print(img)
-    # img is now an in-memory 3D img
-    display = plotting.plot_stat_map(img, alpha=.5, black_bg=True, threshold=3, display_mode="z", cut_coords=1,colorbar=False)
-    display.savefig('figure4_' + '{0:03d}'.format(ii) + '.png')
-
-###############################################################################
-# plotting.show is useful to force the display of figures when running
-# outside IPython
-# plotting.show()
-
-#########################################################################
-# |
-#
-# ______
-#
-# To recap, neuroimaging images (niimgs as we call them) come in
-# different flavors:
-#
-# * 3D images, containing only one brain volume
-# * 4D images, containing multiple brain volumes.
-#
-# More details about the input formats in nilearn for 3D and 4D images is
-# given in the documentation section: :ref:`loading_data`.
-#
-# Functions accept either 3D or 4D images, and we need to use on the one
-# hand :func:`nilearn.image.index_img` or :func:`nilearn.image.iter_img`
-# to break down 4D images into 3D images, and on the other hand
-# :func:`nilearn.image.concat_imgs` to group a list of 3D images into a 4D
-# image.
+if __name__ == "__main__":
+    sys.exit(main())
 
