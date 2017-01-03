2aeee3e2ca7d87b1439cc81a2e3184cb246d058e
bkraft <bkraft4257@gmail.com>
2017-01-03 13:09:36 -0500

    Fixed bugs

--- other/unix/clone_tic.sh
+++ other/unix/clone_tic.sh
@@ -21,7 +21,7 @@ git clone https://github.com/bkraft4257/tic_redcap_link
 if [ ! -d $TIC_SETUP_PATH ]; then
 
     mkdir $TIC_SETUP_PATH
-    cp $TIC_REPOSITORY_PATH/tic_tools/other/unix/{tic,tic_tools}.sh $TIC_SETUP_PATH
+    cp $TIC_REPOSITORY_PATH/tic_tools/other/unix/{tic,tic_tools,tic_aliases,tic_wake_software_environment}.sh $TIC_SETUP_PATH
     cp $TIC_REPOSITORY_PATH/tic_freesurfer/other/tic_freesurfer.sh $TIC_SETUP_PATH
     cp $TIC_REPOSITORY_PATH/tic_redcap_link/other/tic_redcap_link.sh $TIC_SETUP_PATH
 
