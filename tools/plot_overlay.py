cbb280895e01257eb8572364bdb8acb340b499ac
bkraft <bkraft4257@gmail.com>
2017-01-05 13:12:19 -0500

    Added greater control of plot_overlay to be called from the command line.

--- tools/plot_overlay.py
+++ tools/plot_overlay.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python
 
 """
-create volume brain mask from pial surface and aseg
+Plots several overlay images using nilearn.plot_stat_map.
+
+http://nilearn.github.io/modules/generated/nilearn.plotting.plot_stat_map.html
+
 """
 from __future__ import division
 
+import os
 import sys
 import argparse
 from nilearn import plotting, image
-
-
-"""
-Copied from https://gist.github.com/endolith/2879736
-
-Copyright 2012 endolith at gmail com
-Copyright 2009 Ged Ridgway at gmail com
-Translation and modification of
-http://www.mathworks.com/matlabcentral/fileexchange/26026-bipolar-colormap
-Based on Manja Lehmann's hand-crafted colormap for cortical visualisation
-"""
-
-
 from scipy.interpolate import interp1d
 import numpy
-import matplotlib.pyplot as plt
 from matplotlib import cm
 
 
-def bipolar(lutsize=256, n=0.333, interp='linear'):
+
+#
+# bipolar() copied from https://gist.github.com/endolith/2879736
+#
+# Copyright 2012 endolith at gmail com
+# Copyright 2009 Ged Ridgway at gmail com
+# Translation and modification of
+# http://www.mathworks.com/matlabcentral/fileexchange/26026-bipolar-colormap
+# Based on Manja Lehmann's hand-crafted colormap for cortical visualisation
+#
+
+def redblue_bipolar(lutsize=256, n=0.333, interp='linear'):
     """
     Bipolar hot/cold colormap, with neutral central color.
 
@@ -72,26 +73,26 @@ def bipolar(lutsize=256, n=0.333, interp='linear'):
 
     Examples
     --------
-    >>> from mpl_toolkits.mplot3d import Axes3D
-    >>> from matplotlib import cm
-    >>> import matplotlib.pyplot as plt
-    >>> import numpy as np
-    >>> fig = plt.figure()
-    >>> ax = fig.gca(projection='3d')
-    >>> x = y = np.arange(-4, 4, 0.15)
-    >>> x, y = np.meshgrid(x, y)
-    >>> z = (1- x/2 + x**5 + y**3)*exp(-x**2-y**2)
-    >>> surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, linewidth=0.1,
-    >>>                        vmax=abs(z).max(), vmin=-abs(z).max())
-    >>> fig.colorbar(surf)
-    >>> plt.show()
-    >>> set_cmap(bipolar(201))
-    >>> waitforbuttonpress()
-    >>> set_cmap(bipolar(201, 0.1)) # dark gray as neutral
-    >>> waitforbuttonpress()
-    >>> set_cmap(bipolar(201, 0.9)) # light gray as neutral
-    >>> waitforbuttonpress()
-    >>> set_cmap(bipolar(201, 0.5)) # grayscale-friendly colormap
+    # >>> from mpl_toolkits.mplot3d import Axes3D
+    # >>> from matplotlib import cm
+    # >>> import matplotlib.pyplot as plt
+    # >>> import numpy as np
+    # >>> fig = plt.figure()
+    # >>> ax = fig.gca(projection='3d')
+    # >>> x = y = np.arange(-4, 4, 0.15)
+    # >>> x, y = np.meshgrid(x, y)
+    # >>> z = (1- x/2 + x**5 + y**3)*exp(-x**2-y**2)
+    # >>> surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, linewidth=0.1,
+    # >>>                        vmax=abs(z).max(), vmin=-abs(z).max())
+    # >>> fig.colorbar(surf)
+    # >>> plt.show()
+    # >>> set_cmap(bipolar(201))
+    # >>> waitforbuttonpress()
+    # >>> set_cmap(bipolar(201, 0.1)) # dark gray as neutral
+    # >>> waitforbuttonpress()
+    # >>> set_cmap(bipolar(201, 0.9)) # light gray as neutral
+    # >>> waitforbuttonpress()
+    # >>> set_cmap(bipolar(201, 0.5)) # grayscale-friendly colormap
 
     References
     ----------
@@ -138,26 +139,60 @@ def bipolar(lutsize=256, n=0.333, interp='linear'):
     # No form of interpolation works without this, but that means the interpolations are not working right.
     ynew = numpy.clip(ynew, 0, 1)
 
-    return cm.colors.LinearSegmentedColormap.from_list('bipolar', ynew, lutsize)
+    return cm.colors.LinearSegmentedColormap.from_list('redblue_bipolar', ynew, lutsize)
 
 
 # ======================================================================================================================
 # region Main Function
 #
 
-def plot_overlay(background, overlay, xyz_direction, ncuts, threshold=1, alpha=0.5, figure_name='figure1'):
+def plot_overlay_ortho(background, overlay, xyz_direction, ncuts, out_dir=os.getcwd(), **kwargs):
+    """ Creates a nCut png images from the NIFTI image """
 
-    cuts = plotting.find_cut_slices(image.load_img(background), direction=xyz_direction, n_cuts=ncuts, spacing='auto')
+    # Create output directory if it doesn't exist
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-    print(cuts)
-    n_slices = list(range(1,ncuts))
-    print(n_slices)
 
+    output_file  = os.path.abspath( os.path.join(out_dir, 'overlay_{0}'.format(xyz_direction) + '.png'))
+
+    plotting.plot_stat_map(overlay, bg_img=background, cmap=redblue_bipolar(),
+                           display_mode=xyz_direction, cut_coords=(0,0,0),
+                           output_file=output_file,
+                           **kwargs
+                           )
+
+#endregion
+
+
+# ======================================================================================================================
+# region Main Function
+#
+
+def plot_overlay_xyz(background, overlay, xyz_direction, cut_coords, out_dir=os.getcwd(), **kwargs):
+    """ Creates a nCut png images from the NIFTI image """
+
+    # Create output directory if it doesn't exist
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # Loop over cuts and create background image
+
+    if len(cut_coords) == 1:
+        cuts = plotting.find_cut_slices(image.load_img(background), direction=xyz_direction,
+                                        n_cuts=cut_coords[0], spacing='auto')
+    else:
+        cuts = cut_coords
 
     for ii,jj in enumerate(cuts):
-        plotting.plot_stat_map(overlay, bg_img=background, cmap=bipolar(), black_bg=True,
-                                        display_mode=xyz_direction, cut_coords=[jj], threshold=threshold, alpha = alpha,
-                                        output_file='overlay_{0}_{1:03}'.format(xyz_direction, ii) + '.png')
+
+        output_file  = os.path.abspath( os.path.join(out_dir, 'overlay_{0}_{1:03}'.format(xyz_direction, ii) + '.png'))
+
+        plotting.plot_stat_map(overlay, bg_img=background, cmap=redblue_bipolar(),
+                               display_mode=xyz_direction, cut_coords=[jj],
+                               output_file=output_file,
+                               **kwargs
+                               )
 
 #endregion
 
@@ -166,22 +201,168 @@ def plot_overlay(background, overlay, xyz_direction, ncuts, threshold=1, alpha=0
 #
 
 def main():
-    ## Parsing Arguments
-    #
-    #
+    """Parameters:
+
+    bg_img : Niimg-like object
+        The background image that the ROI/mask will be plotted on top of. If nothing is specified, the MNI152
+        template will be used. To turn off background image, just pass “bg_img=False”.
+
+    overlay: Niimg-like object
+       The statistical map image
+
+    cut_coords : None, a tuple of floats, or an integer
+        The MNI coordinates of the point where the cut is performed If display_mode is ‘ortho’,
+        this should be a 3-tuple: (x, y, z) For display_mode == ‘x’, ‘y’, or ‘z’, then these are the coordinates of
+        each cut in the corresponding direction. If None is given, the cuts is calculated automaticaly.
+        If display_mode is ‘x’, ‘y’ or ‘z’, cut_coords can be an integer, in which case it specifies the
+        number of cuts to perform
+
+    output_file : string, or None, optional
+        The name of an image file to export the plot to. Valid extensions are .png, .pdf, .svg. If output_file is
+        not None, the plot is saved to a file, and the display is closed.
+
+    display_mode : {‘ortho’, ‘x’, ‘y’, ‘z’, ‘yx’, ‘xz’, ‘yz’}
+        Choose the direction of the cuts: ‘x’ - sagittal,  ‘y’ - coronal, ‘z’ - axial,
+        ‘ortho’ - three cuts are performed in orthogonal directions.
+
+    colorbar : boolean, optional
+        If True, display a colorbar on the right of the plots.
+
+    figure : integer or matplotlib figure, optional
+        Matplotlib figure used or its number. If None is given, a new figure is created.
+
+    axes : matplotlib axes or 4 tuple of float: (xmin, ymin, width, height), optional
+        The axes, or the coordinates, in matplotlib figure space, of the axes used to display the plot. If None,
+        the complete figure is used.
+
+    title : string, optional
+        The title displayed on the figure.
+
+    threshold : a number, None, or ‘auto’
+        If None is given, the image is not thresholded. If a number is given, it is used to threshold the image:
+        values below the threshold (in absolute value) are plotted as transparent. If auto is given, the threshold
+        is determined magically by analysis of the image.
+
+    annotate : boolean, optional
+        If annotate is True, positions and left/right annotation are added to the plot.
+
+    draw_cross : boolean, optional
+        If draw_cross is True, a cross is drawn on the plot to indicate the cut plosition.
+
+    black_bg : boolean, optional
+        If True, the background of the image is set to be black. If you wish to save figures with a black background,
+        you will need to pass “facecolor=’k’, edgecolor=’k’” to matplotlib.pyplot.savefig.
+
+    cmap : matplotlib colormap, optional
+        The colormap for specified image. The ccolormap must be symmetrical.
+
+    symmetric_cbar : boolean or ‘auto’, optional, default ‘auto’
+        Specifies whether the colorbar should range from -vmax to vmax or from vmin to vmax. Setting to ‘auto’
+        will select the latter if the range of the whole image is either positive or negative. Note: The colormap
+        will always be set to range from -vmax to vmax.
+
+    dim : float, ‘auto’ (by default), optional
+        Dimming factor applied to background image. By default, automatic heuristics are applied based upon the
+        background image intensity. Accepted float values, where a typical scan is -1 to 1 (-1 = increase constrast;
+        1 = decrease contrast), but larger values can be used for a more pronounced effect. 0 means no dimming.
+
+    vmax : float
+        Upper bound for plotting, passed to matplotlib.pyplot.imshow
+"""
 
     usage = "usage: %prog [options] arg1 arg2"
 
-    parser = argparse.ArgumentParser(prog='create_pial_mask')
+    parser = argparse.ArgumentParser(prog='plot_overlay')
 
-    parser.add_argument('background', help="Background Image")
+
     parser.add_argument('overlay', help="Overlay Image")
-    parser.add_argument('--dir', help="Direction", choices=['x','y','z','ortho'], default='z')
-    parser.add_argument('--nslices', help="Number of slices",type=int, default=10)
+    parser.add_argument('--bg_img', help="Background Image")
+
+    parser.add_argument('--out_dir', help="Background Image", default=os.getcwd() )
+    parser.add_argument('--display_mode', help="Direction", choices=['x','y','z', 'ortho'], default='z')
+
+    parser.add_argument('--cut_coords', help="Number of slices", nargs='*', type=int, default=1)
+
+    parser.add_argument('--colorbar', help='If True, display a colorbar on the right of the plots.',
+                        action="store_true", default=False )
+
+    parser.add_argument('--cmap', help=('Selects colormap for overlay image from a set list'
+                                        '[redblue_bipolar]'), default='redblue_bipolar' )
+
+    parser.add_argument('--annotate',
+                        help= ('If annotate is True, positions and left/right '
+                               'annotation is added to the plot.'),
+                        action="store_true", default=False)
+
+    parser.add_argument('--draw_cross', help= ('If draw_cross is True, a cross is drawn on the plot to '
+                                               'indicate the cut plosition.'),
+                        action="store_true", default=False)
+
+    parser.add_argument('--alpha', help='Alpha of overlay image', type=float, default=.8)
+
+    parser.add_argument('--vmax', help='Upper bound for plotting, passed to matplotlib.pyplot.imshow',
+                        type=float, default=.8)
+
+    parser.add_argument('--dim', help=('Dimming factor applied to background image. By default, automatic heuristics'
+                                       'are applied based upon the background image intensity. Accepted float values,'
+                                       'where a typical scan is -1 to 1 (-1 = increase constrast; '
+                                       '1 = decrease contrast), but larger values can be used for a more pronounced'
+                                       'effect. 0 means no dimming.'),
+                        type=float, default=0)
+
+    parser.add_argument('--threshold', help=('If None is given, the image is not thresholded. '
+                                             'If a number is given, it is used to threshold the image: '
+                                             'values below the threshold (in absolute value) '
+                                             'are plotted as transparent. If auto is given, the threshold '
+                                             'is determined magically by analysis of the image.'),
+                        default=None)
+
+    parser.add_argument('--black_bg', help=('If True, the background of the image is set to be black.'
+                                            'If you wish to save figures with a black background,'
+                                            'you will need to pass facecolor=k, edgecolor=k to '
+                                            'matplotlib.pyplot.savefig.'),
+                        action="store_true", default=False)
+
+    parser.add_argument('-v', '--verbose', help='If True, display a colorbar on the right of the plots.',
+                        action="store_true", default=False )
 
     inArgs = parser.parse_args()
 
-    plot_overlay(inArgs.background, inArgs.overlay, inArgs.dir, inArgs.nslices)
+    if inArgs.verbose:
+        print(inArgs.verbose)
+
+    # Setting the threshold like this is necessary to avoid a bug in nilearn.plot_stat_map
+
+    if inArgs.threshold=='auto' or inArgs.threshold==None:
+        threshold=inArgs.threshold
+    else:
+        threshold = float(inArgs.threshold)
+
+
+
+    if 'ortho' in inArgs.display_mode:
+
+        # Ortho plots a single ortho slice at the center of the volume
+        plot_overlay_ortho(inArgs.bg_img, inArgs.overlay, inArgs.display_mode, inArgs.cut_coords, alpha=inArgs.alpha,
+                           threshold=threshold,
+                           colorbar=inArgs.colorbar, out_dir=inArgs.out_dir,
+                           black_bg=inArgs.black_bg,
+                           annotate=inArgs.annotate,
+                           vmax=inArgs.vmax,
+                           dim=inArgs.dim,
+                           draw_cross=inArgs.draw_cross)
+
+    else:
+
+        # Plots a series of slices
+        plot_overlay_xyz(inArgs.bg_img, inArgs.overlay, inArgs.display_mode, inArgs.cut_coords, alpha = inArgs.alpha,
+                         threshold=threshold,
+                         colorbar=inArgs.colorbar, out_dir=inArgs.out_dir,
+                         black_bg=inArgs.black_bg,
+                         annotate=inArgs.annotate,
+                         vmax=inArgs.vmax,
+                         dim=inArgs.dim,
+                         draw_cross=inArgs.draw_cross)
 
 
 #endregion
