7364877e809fe0d3d53c9da362ff35dbaeac4afc
bkraft <bkraft4257@gmail.com>
2017-01-09 15:29:03 -0500

    Updated code to conform to DRY (Don't Repeat Yourself).

--- other/unix/tic_clone.sh
+++ other/unix/tic_clone.sh
@@ -14,10 +14,15 @@ TIC_TOOLS_PATH=${TIC_REPOSITORY_PATH}/tic_tools/
 
 TIC_SETUP_PATH=$HOME/.tic
 
-git clone https://github.com/bkraft4257/tic_cbf
-git clone https://github.com/bkraft4257/tic_freesurfer
-git clone https://github.com/bkraft4257/tic_labels
-git clone https://github.com/crhamilt/tic_protocol_check
-git clone https://github.com/bkraft4257/tic_redcap_link
+cd $TIC_REPOSITORY_PATH
+
+for ii in tic_cbf tic_freesurfer tic_labels tic_protocol_check tic_redcap_link; do
+    [ -d $TIC_REPOSITORY_PATH/${ii} ] || git clone https://github.com/bkraft4257/${ii}
+done
+
+#git clone https://github.com/bkraft4257/tic_freesurfer
+#git clone https://github.com/bkraft4257/tic_labels
+#git clone https://github.com/crhamilt/tic_protocol_check
+#git clone https://github.com/bkraft4257/tic_redcap_link
 
 source  $TIC_TOOLS_PATH/other/unix/tic_initial_setup.sh
