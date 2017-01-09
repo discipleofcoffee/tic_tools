652790132cf4c8b8ee06e08339bebe79e88e2290
bkraft <bkraft4257@gmail.com>
2017-01-09 14:49:26 -0500

    Fixed path bug.

--- other/unix/tic_clone.sh
+++ other/unix/tic_clone.sh
@@ -10,6 +10,8 @@
 #
 
 TIC_REPOSITORY_PATH=${1-$PWD}
+TIC_TOOLS_PATH=${TIC_REPOSITORY_PATH}/tic_tools/
+
 TIC_SETUP_PATH=$HOME/.tic
 
 git clone https://github.com/bkraft4257/tic_cbf
@@ -18,4 +20,4 @@ git clone https://github.com/bkraft4257/tic_labels
 git clone https://github.com/crhamilt/tic_protocol_check
 git clone https://github.com/bkraft4257/tic_redcap_link
 
-source  $TIC_REPOSITORY_PATH/other/unix/tic_initial_setup.sh
+source  $TIC_TOOLS_PATH/other/unix/tic_initial_setup.sh
