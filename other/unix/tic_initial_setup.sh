8aae199044fdef3d31cc398a39a522acb111d4b5
bkraft <bkraft4257@gmail.com>
2017-01-10 12:37:41 -0500

    Added some comments that are printed out when script is run.

--- other/unix/tic_initial_setup.sh
+++ other/unix/tic_initial_setup.sh
@@ -18,10 +18,30 @@ fi
 
 TIC_SETUP_PATH=$HOME/.tic
 
+#
+#
+#
+
 if [ ! -d $TIC_SETUP_PATH ]; then
+
+    echo
+    echo "Copying tic.sh and tic_wake_software_environment.sh to $HOME/.tic/.  These files can be used as a template"
+    echo "for setting up your account."
+    echo
 
     mkdir $TIC_SETUP_PATH
 
     cp $TIC_TOOLS_PATH/other/unix/example_tic.sh                           $TIC_SETUP_PATH/tic.sh
     cp $TIC_TOOLS_PATH/other/unix/example_tic_wake_software_environment.sh $TIC_SETUP_PATH/tic_wake_software_environment.sh
 fi
+
+#
+echo
+echo "Individual studies need to be added separately. The comamnds to do so are echoed below. I know there is a better"
+echo " way to do this with a python script but for the moment I am being lazy. -BK"
+echo
+echo "tail -10 /cenc/tic/studies/cenc/other/unix/cenc_add_to_tic.sh >> $HOME/.tic/tic.sh"
+echo "tail -10 /cenc/tic/studies/wbi/other/wbi_add_to_tic.sh >> $HOME/.tic/tic.sh"
+echo "tail -13 /cenc/tic/studies/radcore/other/radcore_add_to_tic.sh >> $HOME/.tic/tic.sh"
+echo
+echo
