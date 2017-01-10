7b3b7cfb87d8c8bc39a715a1078f58994981da21
bkraft <bkraft4257@gmail.com>
2017-01-10 12:18:40 -0500

    Updated to use TIC_PATH in script.

--- other/unix/tic_initial_setup.sh
+++ other/unix/tic_initial_setup.sh
@@ -9,13 +9,12 @@
 #
 #
 
-TIC_REPOSITORY_PATH=${1-$PWD}
 TIC_SETUP_PATH=$HOME/.tic
 
 if [ ! -d $TIC_SETUP_PATH ]; then
 
     mkdir $TIC_SETUP_PATH
 
-    cp $TIC_REPOSITORY_PATH/tic_tools/other/unix/example_tic.sh                           $TIC_SETUP_PATH/tic.sh
-    cp $TIC_REPOSITORY_PATH/tic_tools/other/unix/example_tic_wake_software_environment.sh $TIC_SETUP_PATH/tic_wake_software_environment.sh
+    cp $TIC_PATH/tic_tools/other/unix/example_tic.sh                           $TIC_SETUP_PATH/tic.sh
+    cp $TIC_PATH/tic_tools/other/unix/example_tic_wake_software_environment.sh $TIC_SETUP_PATH/tic_wake_software_environment.sh
 fi
