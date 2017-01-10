7dc7fc48bb4c7e0b16a2733ad97af85b16d8aa44
bkraft <bkraft4257@gmail.com>
2017-01-10 12:22:45 -0500

    Updated to use TIC_TOOLS_PATH in script. I also check to see if the directory exists.

--- other/unix/tic_initial_setup.sh
+++ other/unix/tic_initial_setup.sh
@@ -9,12 +9,18 @@
 #
 #
 
+"${TIC_TOOLS_PATH:?Need to set TIC_TOOLS_PATH to the location of TIC_TOOLS}"
+
+if [ ! -f $TIC_TOOLS_PATH/tic_tools_bash_setup.sh ]; then
+    echo "TIC_TOOLS_PATH is not correct. Please try again"
+fi
+
 TIC_SETUP_PATH=$HOME/.tic
 
 if [ ! -d $TIC_SETUP_PATH ]; then
 
     mkdir $TIC_SETUP_PATH
 
-    cp $TIC_PATH/tic_tools/other/unix/example_tic.sh                           $TIC_SETUP_PATH/tic.sh
-    cp $TIC_PATH/tic_tools/other/unix/example_tic_wake_software_environment.sh $TIC_SETUP_PATH/tic_wake_software_environment.sh
+    cp $TIC_TOOLS_PATH/other/unix/example_tic.sh                           $TIC_SETUP_PATH/tic.sh
+    cp $TIC_TOOLS_PATH/other/unix/example_tic_wake_software_environment.sh $TIC_SETUP_PATH/tic_wake_software_environment.sh
 fi
